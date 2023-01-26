cd4c08b0347f2f6236e88cf94ac1d773416852cf
karthik-d <karthikdesingu2000@gmail.com>
2023-01-27 02:14:29 +0530

    configs fixed

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 config = dict(
-    ROOT_PATH = os.path.join(os.path.abspath(__file__), *((os.path.pardir,)*1))
+    ROOT_PATH = os.path.abspath(os.path.join(os.path.split(__file__)[0], *((os.path.pardir,)*0)))
 )
 
 print("[INFO] Configurations set.")
